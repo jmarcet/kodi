c6bc5a2aca1cfdf1fbc69d57364a45ab0ded9f65
Javier Marcet <javier@marcet.info>
2012-11-09 11:24:19 +0100

    pvr addon: Add option to inhibit powersaving if a user is logged in or /tmp/.do_not_suspend exists

--- xbmc/pvr/PVRManager.cpp
+++ xbmc/pvr/PVRManager.cpp
@@ -58,6 +58,10 @@
 #include "guilib/Key.h"
 #include "dialogs/GUIDialogPVRChannelManager.h"
 
+#if !defined(_WIN_32)
+#include <utmpx.h>
+#endif
+
 using namespace std;
 using namespace MUSIC_INFO;
 using namespace PVR;
@@ -1311,7 +1315,7 @@ bool CPVRManager::IsIdle(void) const
   if (!IsStarted())
     return true;
 
-  if (IsRecording() || IsPlaying()) // pvr recording or playing?
+  if (IsRecording() || IsPlaying() || IsUserLogged() || IsSuspendCanceled()) // pvr recording or playing or user logged in?
   {
     return false;
   }
@@ -1328,6 +1332,51 @@ bool CPVRManager::IsIdle(void) const
   }
 
   return true;
+}
+
+bool CPVRManager::IsUserLogged(void) const
+{
+#if !defined(_WIN_32)
+    const bool inhibitIfLogged = CSettings::Get().GetBool("pvrpowermanagement.inhibitiflogged");
+    if (!inhibitIfLogged) return false;
+
+    struct utmpx *ut;
+    bool userLoggedIn = false;
+
+    setutxent();
+    while ((ut = getutxent()) != NULL)
+    {
+      if (ut->ut_type == USER_PROCESS && strcmp(ut->ut_line,"ttyS0") != 0)
+      {
+        userLoggedIn = true;
+        break;
+      }
+    }
+    endutxent();
+
+    return userLoggedIn;
+#else
+    return false;
+#endif
+}
+
+bool CPVRManager::IsSuspendCanceled(void) const
+{
+
+    const bool cancelSuspend = CSettings::Get().GetBool("pvrpowermanagement.cancelsuspend");
+    if (!cancelSuspend) return false;
+
+    bool suspendCanceled = false;
+
+
+    FILE *fp = fopen("/tmp/.do_not_suspend", "r");
+    if(fp)
+    {
+      fclose(fp);
+      suspendCanceled = true;
+    }
+
+    return suspendCanceled;
 }
 
 void CPVRManager::ShowPlayerInfo(int iTimeout)
