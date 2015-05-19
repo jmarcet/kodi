cd7ec3728661dd51c881da20dc135bd535907bff
Javier Marcet <javier@marcet.info>
2015-05-19 19:44:46 +0200

    0002-Allow-audio-passthrough-for-Amlogic-based-devices.patch
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAESink.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAESink.cpp
@@ -93,6 +93,7 @@ AEDeviceType CActiveAESink::GetDeviceType(const std::string &device)
 
 bool CActiveAESink::HasPassthroughDevice()
 {
+#ifndef HAS_LIBAMCODEC
   for (AESinkInfoList::iterator itt = m_sinkInfoList.begin(); itt != m_sinkInfoList.end(); ++itt)
   {
     for (AEDeviceInfoList::iterator itt2 = itt->m_deviceInfoList.begin(); itt2 != itt->m_deviceInfoList.end(); ++itt2)
@@ -103,10 +104,14 @@ bool CActiveAESink::HasPassthroughDevice()
     }
   }
   return false;
+#else
+  return true;
+#endif
 }
 
 bool CActiveAESink::SupportsFormat(const std::string &device, AEDataFormat format, int samplerate)
 {
+#ifndef HAS_LIBAMCODEC
   std::string dev = device;
   std::string dri;
   CAESinkFactory::ParseDevice(dev, dri);
@@ -137,6 +142,9 @@ bool CActiveAESink::SupportsFormat(const std::string &device, AEDataFormat forma
     }
   }
   return false;
+#else
+  return true;
+#endif
 }
 
 enum SINK_STATES
